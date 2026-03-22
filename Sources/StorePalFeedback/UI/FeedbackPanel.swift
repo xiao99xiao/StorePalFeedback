687bcdbae3146ad6e3805cdc2555dfb16222540b
Xiao Xiao <xx2004xiamen@gmail.com>
2026-03-22 18:57:56 +0900

    Increase panel height to 580 to fit all form content

--- Sources/StorePalFeedback/UI/FeedbackPanel.swift
+++ Sources/StorePalFeedback/UI/FeedbackPanel.swift
@@ -4,7 +4,7 @@ import AppKit
 final class FeedbackPanel: NSPanel {
     init() {
         super.init(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 420),
+            contentRect: NSRect(x: 0, y: 0, width: 480, height: 580),
             styleMask: [.titled, .closable, .resizable],
             backing: .buffered,
             defer: false
@@ -18,10 +18,10 @@ final class FeedbackPanel: NSPanel {
         isReleasedWhenClosed = false
         animationBehavior = .utilityWindow
 
-        minSize = NSSize(width: 420, height: 360)
-        maxSize = NSSize(width: 600, height: 600)
+        minSize = NSSize(width: 420, height: 520)
+        maxSize = NSSize(width: 600, height: 800)
 
-        setContentSize(NSSize(width: 480, height: 420))
+        setContentSize(NSSize(width: 480, height: 580))
         center()
     }
 }
